230369feb272cc0fdbec9b0bf335fca81c835b7d
Jim <buissinkjim@gmail.com>
2023-02-20 13:12:42 +0100

    ADD: first steps for profile
    Gets & displays the translations from a user

--- lost-in-translation/src/views/Profile.jsx
+++ lost-in-translation/src/views/Profile.jsx
@@ -1,9 +1,19 @@
+import ProfileActions from "../components/Profile/ProfileActions"
+import ProfileHeader from "../components/Profile/ProfileHeader"
+import ProfileHistory from "../components/Profile/ProfileHistory"
+import { useUser } from "../context/UserContext"
 import withAuth from "../hoc/withAuth"
 
 const Profile = () => {
+    const { user } = useUser()
+
     return (
-        <h1>Profile</h1>
+        <>
+            <h1>Profile</h1>
+            <ProfileHeader username={user.username} />
+            <ProfileActions />
+            <ProfileHistory translations={user.translations} />
+        </>
     )
 }
-
 export default withAuth(Profile)
