3bc650d54a641adf4bb3f2d6388ca32f51305b2c
livings08 <marcob93@hotmail.com>
2023-02-22 09:25:18 +0100

    Add button to reset state
    ADD: Button to reset SetText state

--- lost-in-translation/src/views/Translations.jsx
+++ lost-in-translation/src/views/Translations.jsx
@@ -2,16 +2,14 @@ import { useContext } from "react";
 import { translationAdd } from "../api/translation";
 import TranslationCard from "../components/Translation/TranslationCard";
 import TranslationForm from "../components/Translation/TranslationForm";
-import { ImageContext } from "../context/ImageProvider";
 import { useUser } from "../context/UserContext";
 import withAuth from "../hoc/withAuth";
-
+import { ImageContext } from "../context/ImageProvider";
 // import { storageSave } from "../utils/storage";
 
 const Translations = () => {
   const { user, setUser } = useUser();
   const [imgUrl, setText] = useContext(ImageContext);
-  // setText("");
 
   const handleTranslationClicked = async (translation) => {
     // check if you have translation
@@ -30,6 +28,7 @@ const Translations = () => {
       <h1>Translation</h1>
       <TranslationForm onTranslation={handleTranslationClicked} />
       <TranslationCard transLationImage={imgUrl}></TranslationCard>
+      <button onClick={() => setText("")}>Reset translation</button>
     </>
   );
 };
