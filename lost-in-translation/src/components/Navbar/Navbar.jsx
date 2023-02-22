59cdb2e6fca6d1c01fc77d5579829d32a06668f5
Jim <buissinkjim@gmail.com>
2023-02-22 09:57:45 +0100

    ADD: styling to navbar

--- lost-in-translation/src/components/Navbar/Navbar.jsx
+++ lost-in-translation/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,49 @@
 import { NavLink } from "react-router-dom"
 import { useUser } from "../../context/UserContext"
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Navbar = () => {
+const NavBar = () => {
 
     const { user } = useUser()
 
 
     return (
-        <nav>
-            <ul>
-                <li>Sign Language Translations</li>
-            </ul>
-
-            { user !== null &&
-                <ul>
-                    <li>
-                        <NavLink to="/translations">Translations</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/profile">Profile</NavLink>
-                    </li>
-                </ul>
-            }
-        </nav>
+        <Navbar className="navbar">
+            <Container>
+                <Navbar.Brand >Lost in translations</Navbar.Brand>
+                {user !== null &&
+                    <Nav className="me-auto">
+                        <Nav.Link href="/translations">Translations</Nav.Link>
+                        <Nav.Link href="/profile">Profile</Nav.Link>
+                    </Nav>
+                }
+            </Container>
+        </Navbar>
     )
+
+
+
+
+
+    //     <nav>
+    //         <ul>
+    //             <li>Sign Language Translations</li>
+    //         </ul>
+
+    //         { user !== null &&
+    //             <ul>
+    //                 <li>
+    //                     <NavLink to="/translations">Translations</NavLink>
+    //                 </li>
+    //                 <li>
+    //                     <NavLink to="/profile">Profile</NavLink>
+    //                 </li>
+    //             </ul>
+    //         }
+    //     </nav>
+    // )
 }
 
-export default Navbar
+export default NavBar
