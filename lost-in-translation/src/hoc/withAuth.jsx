2d1985f3a8f08e1e908cf734b1c2a6d88420831f
Jim <buissinkjim@gmail.com>
2023-02-24 11:27:43 +0100

    UPDATE: comments for withAuth

--- lost-in-translation/src/hoc/withAuth.jsx
+++ lost-in-translation/src/hoc/withAuth.jsx
@@ -1,13 +1,21 @@
+// Import required modules and dependencies
 import { Navigate } from "react-router-dom"
 import { useUser } from "../context/UserContext"
 
+// A Higher-Order Component (HOC) that wraps around another component and adds authentication check
 const withAuth = Component => props => {
+    // Get the user state from the UserContext using the useUser custom hook
     const { user } = useUser()
+
+    // If user is authenticated, render the component with props
     if (user !== null) {
         return <Component {...props} />
-    } else {
+    }
+    // Otherwise, redirect to the home page using the Navigate component from react-router-dom
+    else {
         return <Navigate to="/" />
     }
 }
 
+// Export withAuth as the default export of the module
 export default withAuth
