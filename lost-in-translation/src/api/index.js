2d929979c093a7352696d1568a4387965e6e9801
Jim <buissinkjim@gmail.com>
2023-02-24 09:21:49 +0100

    UPDATE: comments for API folder

--- lost-in-translation/src/api/index.js
+++ lost-in-translation/src/api/index.js
@@ -1,8 +1,11 @@
+// Get the API key from the environment variables
 const apiKey = process.env.REACT_APP_API_KEY
 
+// Export a function to create headers for HTTP requests
 export const createHeaders = () => {
+    // Return an object with headers
     return {
-        'Content-Type': 'application/json',
-        'x-api-key': apiKey
+        'Content-Type': 'application/json',  // Set the content type to JSON
+        'x-api-key': apiKey  // Set the API key as a header
     }
 }
