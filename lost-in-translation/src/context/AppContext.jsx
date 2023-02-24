c412fb1acd7aa8ca6b4137622ddc59c17bdbede1
Jim <buissinkjim@gmail.com>
2023-02-24 11:01:17 +0100

    UPDATE: removed old files & comments for Context components

--- lost-in-translation/src/context/AppContext.jsx
+++ lost-in-translation/src/context/AppContext.jsx
@@ -1,5 +1,8 @@
+// Import required modules and dependencies
 import UserProvider from "./UserContext"
 
+// Component that gives its children access to the user data provided by the UserProvider component
+// All of the components rendered inside AppContext (e.g. its children) can now reach the UserContext
 const AppContext = ({ children }) => {
 
     return (
@@ -9,4 +12,5 @@ const AppContext = ({ children }) => {
     )
 }
 
+// Export the component
 export default AppContext
